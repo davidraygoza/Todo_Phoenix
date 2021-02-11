c638de05f1acf1cf1fcddf6fc7f7a8132b6e1ee1
David Raygoza <david.raygoza.ramirez@gmail.com>
2021-02-11 17:26:25 -0600

    Adding Apollo to consume GraphQL

--- assets/js/TodoApp/index.tsx
+++ assets/js/TodoApp/index.tsx
@@ -1,7 +1,14 @@
 import React from "react"
-
+import client from "./client"
+import {ApolloProvider} from "@apollo/react-hooks"
+import TodoList from "./TodoList"
 const TodoApp = () => {
-    return <h1> Hello World from REact</h1>
+    return (
+        <ApolloProvider client={client}>
+            <TodoList />
+        </ApolloProvider>
+       
+    )
 }
  
 export default TodoApp
