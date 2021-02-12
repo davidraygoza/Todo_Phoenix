1f3ed585289a6de77b242836cde02b75ac0bed1a
David Raygoza <david.raygoza.ramirez@gmail.com>
2021-02-11 18:42:54 -0600

    Adding style to checkbox

--- assets/js/TodoApp/TodoList.tsx
+++ assets/js/TodoApp/TodoList.tsx
@@ -1,13 +1,8 @@
 import React from "react"
 import {gql} from "apollo-boost"
 import { useQuery } from "@apollo/react-hooks"
-
-interface TodoItem{
-    id: number | string;
-    content: string;
-    isCompleted: boolean;
-}
-
+import TodoItem from './types/TodoItem'
+import TodoListItem from './TodoListItem'
 interface TodoItemsQueryResult {
     TodoItems: TodoItem[];
 }
@@ -22,15 +17,11 @@ const TodoList = () => {
     console.log({"todoItem":data})
     return <div className="todo_list">
         <h3 className="todo_list__header">Todo Items</h3>
-            <ul className="todo_list__list">
-                {data?.todoItems
-                ? data.todoItems.map((item) =>(
-                    <li key={item.id} className={item.isCompleted ? "todo_list__item todo_list_item__completed completed":"todo_list__item"}>
-                        {item.content}
-                    </li>
-                ))
-                :null}               
-            </ul>        
+        <div className="todo_list__list">
+            {data?.todoItems?.map((item: TodoItem) => (
+                <TodoListItem key={item.id} {...item} />
+            ))}
+        </div>       
     </div>
 }
 
